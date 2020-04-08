638fb77ebe64e6eabe0713153f5b0df836bcecf7
Deepak Bhamla <dbhamla7@gmail.com>
2020-04-08 21:36:32 +0530

    background gradient js library

---
+++ script.js
@@ -0,0 +1,18 @@
+var color1 = document.getElementById("color1")
+var color2 = document.getElementById("color2");
+var body = document.getElementById("body");
+var h3 = document.querySelector("h3");
+
+function Gradient(){
+    body.style.background = 
+    "linear-gradient(to right, "
+    + color1.value
+    +", "
+    +color2.value
+    +")";
+    h3.textContent ="copy : "+ body.style.background + ";";
+}
+
+color1.addEventListener("input", Gradient);
+color2.addEventListener("input", Gradient);
+
